adb1bf09b254b2ebdef7ee3d4a345d487de06549
Sameeksha Gupta <136142214+Sameekksshhaa@users.noreply.github.com>
2024-07-12 16:52:15 +0530

    modified register page

--- client/src/layout/index.js
+++ client/src/layout/index.js
@@ -8,7 +8,7 @@ const AuthLayouts = ({children}) => {
             <img 
               src={logo}
               alt='logo'
-              width={180}
+              width={100}
               height={60}
             />
         </header>
